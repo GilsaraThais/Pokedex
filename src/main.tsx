0e396d2cd81f99ad7e65b84c69e4c3a5cc915c5f
Gilsara Moreno <gilsaramore004@gmail.com>
2024-03-20 16:37:55 -0300

    add project routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { Reset } from "./styles/reset";
 import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { App } from "./pages/App";
+import { AppRoutes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <ThemeProvider theme={appTheme}>
-        <App />
+        <AppRoutes/>
         <Reset />
       </ThemeProvider>
     </QueryClientProvider>
---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { App } from "../pages/App";
+import { SearchPage } from "../pages/SearchPage";
+import { Details } from "../pages/Details";
+import { Home } from "../pages/Home";
+import { Page404 } from "../pages/Page404";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Page404/>,
+    children: [
+      {path: "/", element: <Home />},
+      {path: "/details", element: <Details/>},
+      {path: "/search", element: <SearchPage/>},
+   
+    ]
+  },
+]);
+
+export function AppRoutes() {
+  return <RouterProvider router={router} />;
+}
