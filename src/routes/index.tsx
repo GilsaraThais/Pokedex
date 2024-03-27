2f53ce27373826197febe9713a3d1db56fc566e3
Gilsara Moreno <gilsaramore004@gmail.com>
2024-03-27 16:02:28 -0300

    add navigation and validation route params and query params

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,20 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { App } from "../pages/App";
-import { SearchPage } from "../pages/SearchPage";
-import { Details } from "../pages/Details";
 import { Home } from "../pages/Home";
+import { Details } from "../pages/Details";
+import { SearchPage } from "../pages/SearchPage";
 import { Page404 } from "../pages/Page404";
-
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <Page404/>,
+    errorElement: <Page404 />,
     children: [
-      {path: "/", element: <Home />},
-      {path: "/details", element: <Details/>},
-      {path: "/search", element: <SearchPage/>},
-   
-    ]
+      { path: "/", element: <Home /> },
+      { path: "/details/:name", element: <Details /> },
+      { path: "/search", element: <SearchPage /> },
+    ],
   },
 ]);
 
