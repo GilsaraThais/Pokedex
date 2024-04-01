9b630f51ccf5f7996519c3520272bad72fe38d83
Gilsara Moreno <gilsaramore004@gmail.com>
2024-04-01 16:40:06 -0300

    add submit form header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,52 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Container } from "./style";
+import { SubmitHandler, useForm } from "react-hook-form";
 import pokemonLogo from "../../assets/pokemon-logo.png";
 
+type Inputs = {
+  inputSearch: string;
+};
+
 export function Header() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    navigate(`/search?q=${data.inputSearch}`);
+    reset();
+  };
+
   return (
     <Container>
-      <Link to={"/"}>
+      <Link to={"/?page=1"}>
         <img
           src={pokemonLogo}
-          alt="logo amerela com borda azul escrita o texto pokémon"
+          alt="logo com texto escrito pokémon, cor amarela e borda azul"
         />
       </Link>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <section>
           <label htmlFor="inputSearch" hidden>
             Pesquisar Pokémon
           </label>
 
-          <input type="text" id="inputSearch" placeholder="Pesquisar Pokémon" />
+          <input
+            type="text"
+            id="inputSearch"
+            placeholder="Pesquisar Pokémon"
+            {...register("inputSearch", {
+              required: "Preencha o nome do pokémon",
+            })}
+          />
+
+          <span className="inputError">{errors.inputSearch?.message}</span>
         </section>
 
         <button type="submit">Pesquisar</button>
